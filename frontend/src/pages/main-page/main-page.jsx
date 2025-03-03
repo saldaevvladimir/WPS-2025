950d8d11f5646d1fbac02389e8bda82ea6e52a2e
saldaevvladimir <caldaevvlad3@gmail.com>
2025-03-03 19:46:01 +1000

    integrate websocket for RT image update in video player
    update main page and video player

--- frontend/src/pages/main-page/main-page.jsx
+++ frontend/src/pages/main-page/main-page.jsx
@@ -9,6 +9,7 @@ import SeparatorLine from '../../components/common/separator-line/separator-line
 
 function MainPage() {
 	const [stats, setStats] = useState([])
+	const [imageSrc, setImageSrc] = useState(null)
 	const alertsPanelRef = useRef(null)
 
 	const videoSrc = 'videoSrc.m3u8'
@@ -36,15 +37,62 @@ function MainPage() {
 		}
 	}
 
+	function updateStats(stats) {
+		setStats(stats)
+	}
+
+	function updateAlerts(alerts) {
+		alerts.forEach((alert) => {
+			alertsPanelRef.current.addAlert(alert)
+		})
+	}
+
+	function updateData(data) {
+		const stats = [{
+			'name': 'Количество особей',
+			'value': data['population_size'],
+		}]
+		const alerts = data['alerts'] || []
+
+		if (data['frame_with_boxes']) {
+			setImageSrc(data['frame_with_boxes'])
+		}
+
+		updateStats(stats)
+		updateAlerts(alerts)
+	}
+
+	async function setupWebSocket() {
+		const socket = new WebSocket(`ws://127.0.0.1:8000/ws/laboratory/`)
+
+		socket.onmessage = (event) => {
+			const data = JSON.parse(event.data).state
+			console.log('WebSocket data:', data)
+
+			updateData(data)
+		}
+
+		socket.onopen = () => {
+			console.log('WebSocket connection opened')
+		}
+
+		socket.onclose = () => {
+			console.log('WebSocket connection closed')
+		}
+
+		socket.onerror = (error) => {
+			console.error('WebSocket error:', error)
+		}
+	}
+
 	useEffect(() => {
-		updateStatsData(statsUrl)
-		updateAlertsData(alertsUrl)
+		setupWebSocket()
 	}, [])
 
 	return (
 		<div className='MainPage'>
 			<div className='HBox'>
-				<VideoPlayer videoSrc={videoSrc} />
+				<VideoPlayer videoSrc={videoSrc} imageSrc={imageSrc} />
 				<SeparatorLine
 					vertical={true}
 					className='Separator ml-[40px] mr-[40px]'
